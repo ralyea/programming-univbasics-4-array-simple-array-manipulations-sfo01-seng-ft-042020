207955f518e30ca1ed76163bbaaefdd80c2ff612
Paul Ralyea <047par@gmail.com>
2020-03-07 02:30:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,10 @@ def shift_with_args(array)
   array.shift(2)
 end
 
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
 def using_insert(array,element)
   array.insert(4, element)
 end
